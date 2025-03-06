56f453d49b61f2c0f3d49923233779a814f401bd
fthann <fathanalfrq@gmail.com>
2025-03-06 14:26:37 +0700

    final changes

--- App.java
+++ App.java
@@ -24,53 +24,53 @@ public class App {
         // // tampilkan hasil
         // System.out.println("Rata rata nilai adalah: " + rataRata);
 
-        // Matriks A
-        int[][] matriksA = {
-            {1,2},
-            {3,4}
+         // Matriks A
+         int[][] matriksA = {
+            {1, 2},
+            {3, 4}
         };
 
         // Matriks B
         int[][] matriksB = {
-            {5,6},
-            {7,8}
-        };    
+            {5, 6},
+            {7, 8}
+        };
 
-
-        // matriks hasil pennjumlahan
+        // Matriks hasil penjumlahan
         int[][] hasil = new int[2][2];
 
-        // proses penjumlahan
+        // Proses penjumlahan
         for (int baris = 0; baris < matriksA.length; baris++) {
             for (int kolom = 0; kolom < matriksA[baris].length; kolom++) {
                 hasil[baris][kolom] = matriksA[baris][kolom] + matriksB[baris][kolom];
             }
         }
 
-        // tampilkan matriks a
-        System.out.println("Matriks A");
+        // Tampilkan Matriks A
+        System.out.println("Matriks A:");
         for (int[] baris : matriksA) {
             for (int angka : baris) {
-                System.out.println(angka + " ");
+                System.out.print(angka + " ");
             }
             System.out.println();
         }
 
-        // tampilkan matriks B
-        System.out.println("\nMatriks B");
+        // Tampilkan Matriks B
+        System.out.println("\nMatriks B:");
         for (int[] baris : matriksB) {
             for (int angka : baris) {
-                System.out.println(angka + " ");
+                System.out.print(angka + " ");
             }
             System.out.println();
         }
 
-        // tampilkan hasil penjumlahan
-        System.out.println("\nHasil Penjumlahan: ");
+        // Tampilkan Hasil Penjumlahan
+        System.out.println("\nHasil Penjumlahan:");
         for (int[] baris : hasil) {
             for (int angka : baris) {
-                System.out.println(angka + " ");
+                System.out.print(angka + " ");
             }
+            System.out.println();
         }
     }
 }
